2c658e307ce4ee95d8cf03ba9152121c105231c5
ignis2497 <ignis2497@gmail.com>
2017-12-28 11:58:56 +0200

    better training script

--- train.py
+++ train.py
@@ -8,18 +8,30 @@ DATA_AMOUNT = 100
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('epochs', default=EPOCHS, nargs='?',
+parser.add_argument('epochs', default=EPOCHS, nargs='?', type=int,
                     help=f'Amount of epochs to train. Default: {EPOCHS}')
-parser.add_argument('step', default=STEP, nargs='?',
+parser.add_argument('step', default=STEP, nargs='?', type=int,
                     help=f'Amount of train epochs before saving weights. Default: {STEP}')
-parser.add_argument('data_amount', default=DATA_AMOUNT, nargs='?',
+parser.add_argument('data_amount', default=DATA_AMOUNT, nargs='?', type=int,
                     help=f'Amount of data to train with each epoch. Default: {DATA_AMOUNT}')
+
+
+def train(model, epochs, step, data_amount):
+    for _ in range(epochs // step):
+        model.train(data_amount, step)
+        model.save_weights()
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
 
-    sf = CardFinder()
-    sf.init_model()
-    for i in range(args.epochs//args.step):
-        sf.train(args.data_amount, args.step)
-        sf.save_weights()
+    m = CardFinder()
+    m.init_model()
+
+    try:
+        train(m, args.epochs, args.step, args.data_amount)
+    except KeyboardInterrupt as e:
+        m.save_weights()
+        raise e
+
+
