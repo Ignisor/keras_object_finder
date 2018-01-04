a14666e003b0fb7130ad0cb9673945f0b1b5c95d
ignis2497 <ignis2497@gmail.com>
2018-01-04 15:31:53 +0200

    validation

--- validate.py
+++ validate.py
@@ -3,26 +3,39 @@ import numpy as np
 
 from card_finder.model import CardFinder
 
+SAMPLES = 2
+
 if __name__ == '__main__':
     sf = CardFinder()
     sf.init_model()
 
-    tx, ty = sf.dataset_provider.get_batch(10)
+    tx, ty = sf.dataset_provider.get_batch(SAMPLES)
     py = sf.model.predict(tx)
-    pb = py * tuple((*sf.img_size, *sf.img_size) for i in range(len(py)))
 
-    for i, img in enumerate(tx):
-        img = Image.fromarray(np.uint8(img))
+    for i in range(SAMPLES):
+        img = Image.fromarray(np.uint8(tx[i])).convert('RGBA')
+        mask = py[i]
+        valid_mask = ty[i]
 
-        x, y, w, h = pb[i]
+        mask_img = np.dstack((
+            mask,
+            np.zeros(mask.shape),
+            np.zeros(mask.shape),
+            np.ones(mask.shape) * 0.5 * mask
+        ))
+        mask_img = Image.fromarray(np.array(mask_img*255, np.uint8))
+        mask_img = mask_img.resize(img.size)
 
-        d = ImageDraw.Draw(img)
-        poly = ((x, y), (x + w, y), (x + w, y + h), (x, y + h))
-        d.polygon(poly, outline=(255, 0, 0))
+        valid_mask_img = np.dstack((
+            np.zeros(valid_mask.shape),
+            valid_mask,
+            np.zeros(valid_mask.shape),
+            np.ones(valid_mask.shape) * 0.25 * valid_mask
+        ))
+        valid_mask_img = Image.fromarray(np.array(valid_mask_img*255, np.uint8))
+        valid_mask_img = valid_mask_img.resize(img.size)
 
-        x, y, w, h = ty[i]
+        composed = Image.alpha_composite(img, mask_img)
+        composed = Image.alpha_composite(composed, valid_mask_img)
 
-        poly = ((x, y), (x + w, y), (x + w, y + h), (x, y + h))
-        d.polygon(poly, outline=(0, 255, 0))
-
-        img.show()
+        composed.show()
