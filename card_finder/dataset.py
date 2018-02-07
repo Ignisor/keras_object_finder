eb3058d4977ce56ea37002466b0a2bddb449df74
ignis2497 <ignis2497@gmail.com>
2018-02-07 17:20:35 +0200

    Abstract generator class Black squares generator

--- card_finder/dataset.py
+++ card_finder/dataset.py
@@ -1,8 +1,74 @@
+import abc
 import os
 import random
 
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
+
+
+class BaseGenerator(abc.ABC):
+    def __init__(self, input_shape=(28, 28, 3), output_shape=(4,), data_amount=10000):
+        super().__init__()
+
+        self.amount = data_amount
+
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+        self.img_w, self.img_h, self.img_ch = self.count_image_sizes()
+
+    def count_image_sizes(self):
+        w = h = ch = None
+        if len(self.input_shape) == 3:
+            w, h, ch = self.input_shape
+        if len(self.input_shape) == 2:
+            size, ch = self.input_shape
+            w = h = size/size
+
+        return w, h, ch
+
+    @abc.abstractmethod
+    def _generate(self):
+        pass
+
+    def get_batch(self, amount=None):
+        amount = amount or self.amount
+        imgs = []
+        borders = []
+        for i in range(amount):
+            img, border = self._generate()
+            imgs.append(img)
+            borders.append(border)
+
+        np_imgs = np.stack(imgs)
+        np_borders = np.stack(borders)
+
+        return np_imgs, np_borders
+
+    def get_generator(self):
+        while True:
+            img, border = self._generate()
+            img = img.reshape(-1, *self.input_shape)
+            border = border.reshape(-1, *self.output_shape)
+
+            yield img, border
+
+    @staticmethod
+    def img_to_numpy(img):
+        channels = BaseGenerator.convert_mode_to_channels(img.mode)
+        result = np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], channels)
+        result = result / 255
+
+        return result
+
+    @staticmethod
+    def convert_mode_to_channels(mode):
+        modes = {
+            'L': 1,
+            'RGB': 3,
+        }
+
+        return modes[mode]
 
 
 class CardImgGenerator(object):
@@ -83,3 +149,31 @@ class CardImgGenerator(object):
         img = Image.open(fpath)
 
         return img
+
+
+class BlackSquaresGenerator(BaseGenerator):
+    def _generate(self):
+        str_ch = ''
+        if self.img_ch == 3:
+            str_ch = 'RGB'
+        elif self.img_ch == 1:
+            str_ch = 'L'
+
+        img = Image.new(str_ch, (self.img_w, self.img_h), color=(255, 255, 255))
+
+        w = random.randint(1, img.width/2)
+        h = random.randint(1, img.height/2)
+        x = random.randint(0, img.width - w)
+        y = random.randint(0, img.height - h)
+
+        draw = ImageDraw.Draw(img)
+        draw.rectangle((x, y, x + w, y + h), fill=(0,) * self.img_ch)
+
+        np_img = BlackSquaresGenerator.img_to_numpy(img)
+
+        w = w/img.width
+        h = h/img.height
+        x = x/img.width
+        y = y/img.height
+
+        return np_img, np.array((x, y, w, h))
